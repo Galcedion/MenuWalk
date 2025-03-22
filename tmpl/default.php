fe52c1ce4589a1c29c4a6d7717c90c21857253d1
Galcedion <galcedion@gmx.de>
2025-03-22 22:25:46 +0100

    adding bootstrap and fontawesome

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,26 +6,27 @@
  * @license    GNU/GPL: https://gnu.org/licenses/gpl.html
  */
 defined('_JEXEC') or die;
-
-$parent = $menu_elements['parent'];
-
 ?>
-<div class="<?=$main_class;?>">
-<div>
-<?php if(isset($menu_elements['first'])): ?>
-<a href="<?=$menu_elements['first']['url'];?>"><?=$menu_elements['first']['title'];?></a>
+<div class="<?=$main_class;?> row">
+<div class="col">
+<?php if(in_array('first', $enabled_buttons) && isset($menu_elements['first'])): ?>
+<a href="<?=$menu_elements['first']['url'];?>" class="btn btn-outline-info"><i class="fa-solid fa-backward-step"></i> <?=$menu_elements['first']['title'];?></a>
 <?php endif; ?>
-<?php if(isset($menu_elements['prev'])): ?>
-<a href="<?=$menu_elements['prev']['url'];?>"><?=$menu_elements['prev']['title'];?></a>
+</div><div class="col">
+<?php if(in_array('prev', $enabled_buttons) && isset($menu_elements['prev'])): ?>
+<a href="<?=$menu_elements['prev']['url'];?>" class="btn btn-outline-info"><i class="fa-solid fa-backward"></i> <?=$menu_elements['prev']['title'];?></a>
 <?php endif; ?>
-</div><div>
-<a href="<?=$parent['url'];?>"><?=$parent['title'];?></a>
-</div><div>
-<?php if(isset($menu_elements['next'])): ?>
-<a href="<?=$menu_elements['next']['url'];?>"><?=$menu_elements['next']['title'];?></a>
+</div><div class="col">
+<?php if(in_array('top', $enabled_buttons)): ?>
+<a href="<?=$menu_elements['parent']['url'];?>" class="btn btn-outline-info"><i class="fa-solid fa-eject"></i> <?=$menu_elements['parent']['title'];?> <i class="fa-solid fa-eject"></i></a>
 <?php endif; ?>
-<?php if(isset($menu_elements['last'])): ?>
-<a href="<?=$menu_elements['last']['url'];?>"><?=$menu_elements['last']['title'];?></a>
+</div><div class="col">
+<?php if(in_array('next', $enabled_buttons) && isset($menu_elements['next'])): ?>
+<a href="<?=$menu_elements['next']['url'];?>" class="btn btn-outline-info"><?=$menu_elements['next']['title'];?> <i class="fa-solid fa-forward"></i></a>
+<?php endif; ?>
+</div><div class="col">
+<?php if(in_array('last', $enabled_buttons) && isset($menu_elements['last'])): ?>
+<a href="<?=$menu_elements['last']['url'];?>" class="btn btn-outline-info"><?=$menu_elements['last']['title'];?> <i class="fa-solid fa-forward-step"></i></a>
 <?php endif; ?>
 </div>
 </div>
