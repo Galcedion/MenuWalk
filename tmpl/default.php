1065f271b3c081fe71c522e996f9bb3a0ed7ce17
Galcedion <galcedion@gmx.de>
2025-03-23 20:42:07 +0100

    making fa and bootstrap optional

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,27 +6,33 @@
  * @license    GNU/GPL: https://gnu.org/licenses/gpl.html
  */
 defined('_JEXEC') or die;
+$button_classlist = $g_mw_config['show_bootstrap'] ? 'class="btn btn-outline-info"' : '';
+$fa_first = $g_mw_config['show_fontawesome'] ? '<i class="fa-solid fa-backward-step"></i>' : '';
+$fa_prev = $g_mw_config['show_fontawesome'] ? '<i class="fa-solid fa-backward"></i>' : '';
+$fa_parent = $g_mw_config['show_fontawesome'] ? '<i class="fa-solid fa-eject"></i>' : '';
+$fa_next = $g_mw_config['show_fontawesome'] ? '<i class="fa-solid fa-forward"></i>' : '';
+$fa_last = $g_mw_config['show_fontawesome'] ? '<i class="fa-solid fa-forward-step"></i>' : '';
 ?>
-<div class="<?=$main_class;?> row">
-<div class="col">
+<div class="<?=$main_class;?> <?=!$g_mw_config['show_bootstrap']?:'row';?>">
+<div class="<?=!$g_mw_config['show_bootstrap']?:'col';?>">
 <?php if(in_array('first', $enabled_buttons) && isset($menu_elements['first'])): ?>
-<a href="<?=$menu_elements['first']['url'];?>" class="btn btn-outline-info"><i class="fa-solid fa-backward-step"></i> <?=$menu_elements['first']['title'];?></a>
+<a href="<?=$menu_elements['first']['url'];?>" <?=$button_classlist;?>><?=$fa_first;?> <?=$menu_elements['first']['title'];?></a>
 <?php endif; ?>
 </div><div class="col">
 <?php if(in_array('prev', $enabled_buttons) && isset($menu_elements['prev'])): ?>
-<a href="<?=$menu_elements['prev']['url'];?>" class="btn btn-outline-info"><i class="fa-solid fa-backward"></i> <?=$menu_elements['prev']['title'];?></a>
+<a href="<?=$menu_elements['prev']['url'];?>" <?=$button_classlist;?>><?=$fa_prev;?> <?=$menu_elements['prev']['title'];?></a>
 <?php endif; ?>
 </div><div class="col">
 <?php if(in_array('top', $enabled_buttons)): ?>
-<a href="<?=$menu_elements['parent']['url'];?>" class="btn btn-outline-info"><i class="fa-solid fa-eject"></i> <?=$menu_elements['parent']['title'];?> <i class="fa-solid fa-eject"></i></a>
+<a href="<?=$menu_elements['parent']['url'];?>" <?=$button_classlist;?>><?=$fa_parent;?> <?=$menu_elements['parent']['title'];?> <?=$fa_parent;?></a>
 <?php endif; ?>
 </div><div class="col">
 <?php if(in_array('next', $enabled_buttons) && isset($menu_elements['next'])): ?>
-<a href="<?=$menu_elements['next']['url'];?>" class="btn btn-outline-info"><?=$menu_elements['next']['title'];?> <i class="fa-solid fa-forward"></i></a>
+<a href="<?=$menu_elements['next']['url'];?>" <?=$button_classlist;?>><?=$menu_elements['next']['title'];?> <?=$fa_next;?></a>
 <?php endif; ?>
 </div><div class="col">
 <?php if(in_array('last', $enabled_buttons) && isset($menu_elements['last'])): ?>
-<a href="<?=$menu_elements['last']['url'];?>" class="btn btn-outline-info"><?=$menu_elements['last']['title'];?> <i class="fa-solid fa-forward-step"></i></a>
+<a href="<?=$menu_elements['last']['url'];?>" <?=$button_classlist;?>><?=$menu_elements['last']['title'];?> <?=$fa_last;?></a>
 <?php endif; ?>
 </div>
 </div>
