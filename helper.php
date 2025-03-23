897f7de37e3c41f55f3b079da3812d3a59253dba
Galcedion <galcedion@gmx.de>
2025-03-23 20:20:28 +0100

    improving helper, making title optional, adding optional count

--- helper.php
+++ helper.php
@@ -9,19 +9,20 @@ defined('_JEXEC') or die;
 
 class ModMenuWalk
 {
-	public static function get_menu_elements() {
+	public static function get_menu_elements($g_mw_config) {
 		$menu_elements = [];
 		$menu = JFactory::getApplication()->getMenu();
 		$current = $menu->getActive();
 		$tmp_parent = $menu->getItem($current->parent_id);
 		$parent_level = $tmp_parent->level;
-		$menu_elements['parent'] = ['title' => $tmp_parent->title, 'url' => JRoute::_($tmp_parent->link)];
+		$menu_elements['parent'] = ModMenuWalk::build_menu_element($tmp_parent, $g_mw_config);
 
 		$menu_full = $menu->getItems(array(), array());
 
 		$passed_current = False; // indicate passed current entry
 		$reached_parent = False;
 		$entries = ['first' => Null, 'prev' => Null, 'next' => Null, 'last' => Null];
+		$count = 0;
 
 		for($i = 0; $i < count($menu_full); ++$i) {
 			if($menu_full[$i]->id == $tmp_parent->id) { // reached the parent, the following entries are the menu to traverse
@@ -29,30 +30,36 @@ class ModMenuWalk
 			} elseif($reached_parent && $menu_full[$i]->level === $tmp_parent->level) { // exiting the child / neighbour scope
 				break;
 			} elseif($reached_parent && $menu_full[$i]->level == $tmp_parent->level + 1) { // only process direct children of parent
+				++$count;
 				if($current->id == $menu_full[$i]->id) {
 					$passed_current = True;
 					if($entries['first'] === Null)
 						$entries['first'] = False;
 				} elseif(!$passed_current) {
-					$entries['prev'] = $menu_full[$i];
+					$entries['prev'] = ModMenuWalk::build_menu_element($menu_full[$i], $g_mw_config, $count);
 					if($entries['first'] === Null)
-						$entries['first'] = $menu_full[$i];
+						$entries['first'] = ModMenuWalk::build_menu_element($menu_full[$i], $g_mw_config, $count);
 				} elseif($passed_current) {
-					$entries['last'] = $menu_full[$i];
+					$entries['last'] = ModMenuWalk::build_menu_element($menu_full[$i], $g_mw_config, $count);
 					if($entries['next'] === Null)
-						$entries['next'] = $menu_full[$i];
+						$entries['next'] = ModMenuWalk::build_menu_element($menu_full[$i], $g_mw_config, $count);
 				}
 			}
 		}
 		if($entries['first'] != NUll)
-			$menu_elements['first'] = ['title' => $entries['first']->title, 'url' => JRoute::_($entries['first']->link)];
+			$menu_elements['first'] = $entries['first'];
 		if($entries['prev'] != NUll)
-			$menu_elements['prev'] = ['title' => $entries['prev']->title, 'url' => JRoute::_($entries['prev']->link)];
+			$menu_elements['prev'] = $entries['prev'];
 		if($entries['next'] != Null)
-			$menu_elements['next'] = ['title' => $entries['next']->title, 'url' => JRoute::_($entries['next']->link)];
+			$menu_elements['next'] = $entries['next'];
 		if($entries['last'] != Null)
-			$menu_elements['last'] = ['title' => $entries['last']->title, 'url' => JRoute::_($entries['last']->link)];
+			$menu_elements['last'] = $entries['last'];
 
 		return $menu_elements;
+	}
+
+	private static function build_menu_element($menu_item, $g_mw_config, $count = Null) {
+		$title = ($g_mw_config['show_menu_count'] ? strval($count) : '') . ' ' . ($g_mw_config['show_menu_title'] ? $menu_item->title : '');
+		return ['title' => $title, 'url' => JRoute::_($menu_item->link)];
 	}
 }
--- mod_menuwalk.php
+++ mod_menuwalk.php
@@ -9,9 +9,14 @@ defined('_JEXEC') or die;
 
 require_once dirname(__FILE__) . '/helper.php';
 
-$menu_elements = ModMenuWalk::get_menu_elements();
-
 $main_class = $params->get('g_class');
 $enabled_buttons = $params->get('enable_buttons');
+$g_mw_config = [];
+$g_mw_config['show_menu_count'] = ($params->get('enable_menu_count') == 1 ? true : false);
+$g_mw_config['show_menu_title'] = ($params->get('enable_menu_title') == 1 ? true : false);
+$g_mw_config['show_fontawesome'] = ($params->get('enable_fontawesome') == 1 ? true : false);
+$g_mw_config['show_bootstrap'] = ($params->get('enable_bootstrap') == 1 ? true : false);
+
+$menu_elements = ModMenuWalk::get_menu_elements($g_mw_config);
 
 require JModuleHelper::getLayoutPath('mod_menuwalk');
